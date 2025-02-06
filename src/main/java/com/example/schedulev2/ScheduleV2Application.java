1290cd998f38c892c584068ebb96c41cca7fcb74
나연 <kny5579@naver.com>
2025-02-06 15:48:25 +0900

    init

---
+++ src/main/java/com/example/schedulev2/ScheduleV2Application.java
@@ -0,0 +1,13 @@
+package com.example.schedulev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleV2Application.class, args);
+    }
+
+}
